bb4e8ebb9d3e634a4d295a133f4b9df4853f96cd
Marco Lölkes <marcoloelkes@gmx.de>
2022-05-04 12:29:06 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Contacts from './contacts.json';
+
+const allContacts = Contacts;
+const firstFiveContacts = allContacts.slice(0,5);
 
 function App() {
+  const [contacts, setContacts] = useState(firstFiveContacts);
+
+  const addContact = () => {
+    const randomNumber = Math.floor(Math.random() * allContacts.length);
+    const contact = allContacts[randomNumber];
+    setContacts([contact, ...contacts]);
+  };
+
+  const sortByPopularity = () => {
+    let sortedContacts = contacts.sort(function (a, b) {
+      return a.popularity > b.popularity;
+    });
+    setContacts([...sortedContacts]);
+  };
+
+  const sortByName = () => {
+    let sortedContacts = contacts.sort(function (a, b) {
+      return a.name.localeCompare(b.name);
+    });
+    setContacts([...sortedContacts]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>IronContacts</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map(contact =>
+          (
+            <tr key={contact.id}>
+              <td><img src={contact.pictureUrl} alt='profile'/></td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity}</td>
+              <td>{contact.wonOscar ? <p>🌞</p> : <p>⚡</p>}</td>
+              <td>{contact.wonEmmy ? <p>🌞</p> : <p>⚡</p>}</td>
+            </tr>
+          )
+          )}
+        </tbody>
+      </table>
+      <button onClick={addContact}>Add random contact</button>
+      <button onClick={sortByPopularity}>Sort by popularity</button>
+      <button onClick={sortByName}>Sort by name</button>
+    </>
   );
 }
 
